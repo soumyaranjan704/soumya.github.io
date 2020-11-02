34eab4f09278a725a5b0d0f707d801c571e36b39
soumyaranjan704 <soumyaranjan.narendra@antronsys.com>
2020-11-02 16:29:58 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LogoutComponent } from './logout/logout.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'logout',component:LogoutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[LogoutComponent]
